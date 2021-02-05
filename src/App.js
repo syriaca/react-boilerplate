66743b9a5466c3625c7ba8fda7532d4fe7b224fc
jean-yves le cabellec <jean-yves.lecabellec@leetchi.com>
2021-02-05 22:29:38 +0100

    Add create react app and tailwind 2.0

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>
+        Simple create-react-app Boilerplate
+      </h1>
+    </div>
+  );
+}
+
+export default App;
